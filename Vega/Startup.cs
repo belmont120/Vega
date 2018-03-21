611fae2b5a4c27cc0e95203227777e602aa13856
Xin Wang <belmont.xwang@gmail.com>
2018-03-20 23:11:06 -0700

    AddConnectionString

--- Vega/Startup.cs
+++ Vega/Startup.cs
@@ -7,6 +7,8 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.Webpack;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Vega.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace Vega
 {
@@ -22,6 +24,8 @@ namespace Vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<VegaDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Default")));
+
             services.AddMvc();
         }
 
