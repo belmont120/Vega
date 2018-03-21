2ce2cff6dec3ac0d9e69511ca5f0dc0225825611
Xin Wang <belmont.xwang@gmail.com>
2018-03-21 02:17:11 -0700

    AddMigrationSeedingFeatures

---
+++ Vega/Migrations/20180321085837_SeedingFeatures.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Vega.Migrations
+{
+    public partial class SeedingFeatures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES('ABS')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES('ESP')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES('Traction Control')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES('V8 Engine')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES('Auto Headlight')");
+            migrationBuilder.Sql("INSERT INTO Features (Name) VALUES('Automatic')");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("TRUNCATE TABLE Features");
+        }
+    }
+}
