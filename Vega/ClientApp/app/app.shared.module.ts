e8b66edec2686c8aaa31f8ddf5f61e4c73a11311
Xin Wang <belmont.xwang@gmail.com>
2018-03-21 03:26:56 -0700

    CreateMakeService

--- Vega/ClientApp/app/app.shared.module.ts
+++ Vega/ClientApp/app/app.shared.module.ts
@@ -10,6 +10,7 @@ import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
 import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { MakeService } from './services/make.service';
 
 
 @NgModule({
@@ -33,7 +34,10 @@ import { CounterComponent } from './components/counter/counter.component';
             { path: 'fetch-data', component: FetchDataComponent },
             { path: '**', redirectTo: 'home' }
         ])
-    ]
+    ],
+    providers: [
+        MakeService,
+    ],
 })
 export class AppModuleShared {
 }
