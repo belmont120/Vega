5102b0c94050fb9fb6f6db6a68b7ffca1fd63ea2
Xin Wang <belmont.xwang@gmail.com>
2018-03-20 23:14:35 -0700

    AddMakeModelToDbContext

--- Vega/Persistence/VegaDbContext.cs
+++ Vega/Persistence/VegaDbContext.cs
@@ -1,9 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using Vega.Models;
 
 namespace Vega.Persistence
 {
     public class VegaDbContext : DbContext
     {
+        public DbSet<Make> Makes { get; set; }
+        public DbSet<Model> Models { get; set; }
         public VegaDbContext(DbContextOptions<VegaDbContext> options)
             : base(options)
         {
